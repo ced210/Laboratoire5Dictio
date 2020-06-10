188a75c3783bd7990ca8092c6545cc6cf7ca4291
Ced-Mich Pro-Tail <37204120+ced210@users.noreply.github.com>
2020-06-10 10:54:58 -0400

    Add LexiNode class

---
+++ Dictio/src/AllTests.java
@@ -0,0 +1,15 @@
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AllTests {
+
+	///public class TestJunit {
+	   @Test
+	   public void testAdd() {
+	      LexiNode lexiNode = new LexiNode('a');
+	      
+	      lexiNode.addChild('v');
+	   }
+	//}
+}
--- Dictio/src/LexiNode.java
+++ Dictio/src/LexiNode.java
@@ -1,8 +1,25 @@
+import java.util.ArrayList;
 
 public class LexiNode {
-
-	public LexiNode() {
-		// TODO Auto-generated constructor stub
+	
+	char mLetter;
+	
+	ArrayList<LexiNode> children;
+	
+	public ArrayList<LexiNode> getChildren() {
+		return this.children;
+	}
+	
+	public char getLetter() {
+		return this.mLetter;
 	}
 
+	public LexiNode(char letter) {
+		// TODO Auto-generated constructor stub
+		this.mLetter = letter;
+	}
+	
+	public void addChild(char letter) {
+		this.children.add(new LexiNode(letter));
+	}
 }
